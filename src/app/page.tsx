e38bd2e72227784d452f8f75cac16caf74fea50b
Kyle Barr <kylebarr54321@gmail.com>
2024-03-05 22:53:08 -0700

    put hero in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Hero from "@/components/main/Hero";
+
 export default function Home() {
   return (
     <div className="flex justify-center">
@@ -8,25 +10,6 @@ export default function Home() {
             test
           </div>
         </div>
-      </div>
-    </div>
-  );
-}
-
-function Hero() {
-  return (
-    <div className="p-16 lg:grid lg:grid-cols-2 flex flex-col gap-8">
-      <div className="flex md:flex-row flex-col gap-6">
-        <img src="/img/pfp.jpg" className="h-32 w-32 rounded-full" />
-        <div className="flex flex-col justify-center gap-5">
-          <span className="text-5xl font-semibold">Kyle Barr</span>
-          <div className="flex flex-col text-gray-200">
-            <span className="text-xl">Frontend Engineer & 3D Artist</span>
-          </div>
-        </div>
-      </div>
-      <div className="text-gray-400 text-lg flex items-center">
-        <span>I build performant interfaces with user experience in mind, as well as high quality 3D assets and renders.</span>
       </div>
     </div>
   );
---
+++ src/components/main/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+    return (
+      <div className="p-16 lg:grid lg:grid-cols-2 flex flex-col gap-8">
+        <div className="flex md:flex-row flex-col gap-6">
+          <img src="/img/pfp.jpg" className="h-32 w-32 rounded-full" />
+          <div className="flex flex-col justify-center gap-5">
+            <span className="text-5xl font-semibold">Kyle Barr</span>
+            <div className="flex flex-col text-gray-200">
+              <span className="text-xl">Frontend Engineer & 3D Artist</span>
+            </div>
+          </div>
+        </div>
+        <div className="text-gray-400 text-lg flex items-center">
+          <span>I build performant interfaces with user experience in mind, as well as high quality 3D assets and renders.</span>
+        </div>
+      </div>
+    );
+  }
