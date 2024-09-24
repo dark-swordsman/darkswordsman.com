640497d26b0b34f942c369232bad7e771640a7a1
Kyle Barr <kylebarr54321@gmail.com>
2024-09-24 17:03:46 -0600

    echo project page 1

--- src/app/projects/echo-home-world/page.tsx
+++ src/app/projects/echo-home-world/page.tsx
@@ -1,7 +1,36 @@
+import Link from "next/link";
+import SecondaryCard from "../../../components/SecondaryCard";
+import ContentLink from "../../../components/ContentLink";
+import Image from "next/image";
+
 export default function Project() {
   return (
     <div>
-      echo home world
+      <SecondaryCard>
+        <div className="flex flex-col gap-4">
+          <span className="text-2xl font-semibold">Echo's Home World</span>
+          <Image className="rounded-lg" src="/img/echo-home-world/echo-5.jpg" alt="Echo's Home World" width={1920} height={1080} />
+          <span>
+            <ContentLink newTab href="https://vrchat.com/home/world/wrld_f5519dc9-4ed2-4dc0-b9b4-431355bf87c1">Echo's Home World</ContentLink> is a VRChat world I created for my friend <ContentLink newTab href="https://x.com/Echo_DNG">Echo_DNG</ContentLink>.
+          </span>
+          <span>
+            The primary goal of this world was to be a hangout spot for Echo and his friends, as well as
+            showcase his custom avatars.
+          </span>
+          <span>
+            We worked together to compile notes of what he wanted in the world and what sort of vibe to target.
+            Echo also gave me a few reference worlds to visit and take inspiration from. 
+          </span>
+          <div className="grid grid-cols-3 grid-rows-2 gap-3">
+            <Image className="rounded-lg" src="/img/echo-home-world/echo-1.jpg" alt="Echo's Home World" width={1920} height={1080} />
+            <Image className="rounded-lg" src="/img/echo-home-world/echo-2.jpg" alt="Echo's Home World" width={1920} height={1080} />
+            <Image className="rounded-lg" src="/img/echo-home-world/echo-3.jpg" alt="Echo's Home World" width={1920} height={1080} />
+            <Image className="rounded-lg" src="/img/echo-home-world/echo-4.jpg" alt="Echo's Home World" width={1920} height={1080} />
+            <Image className="rounded-lg" src="/img/echo-home-world/echo-5.jpg" alt="Echo's Home World" width={1920} height={1080} />
+            <Image className="rounded-lg" src="/img/echo-home-world/echo-6.jpg" alt="Echo's Home World" width={1920} height={1080} />
+          </div>
+        </div>
+      </SecondaryCard>
     </div>
   );
 }
--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function ProjectsLayout({ children }: { children: string }) {
   return (
-    <div className="flex flex-col gap-8">
-      <Link href="/" className="text-lg hover:underline underline-offset-4 w-fit rounded-lg px-3 py-1 bg-zinc-700 hover:bg-zinc-600">{"< Return To Home"}</Link>
+    <div className="flex flex-col gap-6">
+      <Link href="/" className="hover:underline underline-offset-4 w-fit rounded-lg px-3 py-1">{"< Return To Home"}</Link>
       {children}
     </div>
   );
